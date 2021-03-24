6d17bbf4c9ab05749d4268eaa84380b0f7d8e962
stescobedo92 <stescobedo.31@gmail.com>
2021-03-23 21:29:53 -0400

    Download files on website and convert into a epub

---
+++ command/root.go
@@ -0,0 +1,28 @@
+package command
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "linkstobook",
+	Short: "A simple tool to convert web pages into epub",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
